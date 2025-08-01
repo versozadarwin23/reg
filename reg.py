01f18bd140ae5c31c586957063cc9898e0d4773f
versozadarwin23 <109713017+versozadarwin23@users.noreply.github.com>
2025-08-02 00:22:49 +0800

    Update reg.py

--- reg.py
+++ reg.py
@@ -3,6 +3,8 @@ import json
 import os
 import atexit
 import hashlib
+
+import uuid
 from requests.utils import dict_from_cookiejar
 from openpyxl import Workbook, load_workbook
 import requests
@@ -11,7 +13,17 @@ import time
 import random
 from zipfile import BadZipFile
 
-COOKIE_DIR = "/storage/emulated/0/cookie"
+
+COLOR_GREEN = '\033[92m'
+COLOR_RESET = '\033[0m'
+
+def clear_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+COOKIE_DIR = "cookie"
 CONFIG_FILE = "settings.json"
 
 def random_device_model():
@@ -387,7 +399,7 @@ def save_session_cookie(session):
     save_cookie_json(cookie_dict)
 
 def create_fbunconfirmed(account_type, usern, gender, password=None, session=None):
-    global custom_password_base
+    global custom_password_base, token
     agent = random.choice(ua)
 
     if password is None:
@@ -556,13 +568,110 @@ def create_fbunconfirmed(account_type, usern, gender, password=None, session=Non
 
     uid = session.cookies.get("c_user")
     profile_id = f'https://www.facebook.com/profile.php?id={uid}'
-    filename_xlsx = "/storage/emulated/0/Acc_Created.xlsx"
-    filename_txt = "/storage/emulated/0/Acc_created.txt"
+    filename_xlsx = "Acc_Created.xlsx"
+    filename_txt = "Acc_created.txt"
 
     while True:
-        save_confirm = input("Do you want to save this account information? (y/n): ").lower()
+        save_confirm = input("Do you want to save this account (y/n): ").lower()
         if save_confirm == 'y':
-            data_to_save = [full_name, email_or_phone, password, profile_id]  # Assuming these variables are defined elsewhere in your code
+
+            clear_console()
+
+            r = requests.Session()
+
+            r = requests.Session()
+
+            COLOR_GREEN = '\033[92m'
+            COLOR_RED = '\033[91m'
+            COLOR_YELLOW = '\033[93m'
+            COLOR_CYAN = '\033[96m'  # New color for exit message
+            COLOR_RESET = '\033[0m'  # Resets the color to default
+            head = {
+                'Host': 'b-graph.facebook.com',
+                'X-Fb-Connection-Quality': 'EXCELLENT',
+                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) vivo Y93 Build/O11019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/138.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/496.0.0.45.65;IABMV/1;]',
+                'X-Tigon-Is-Retry': 'false',
+                'X-Fb-Friendly-Name': 'authenticate',
+                'X-Fb-Connection-Bandwidth': str(random.randrange(70000000, 80000000)),
+                'Zero-Rated': '0',
+                'X-Fb-Net-Hni': str(random.randrange(50000, 60000)),
+                'X-Fb-Sim-Hni': str(random.randrange(50000, 60000)),
+                'X-Fb-Request-Analytics-Tags': '{"network_tags":{"product":"350685531728","retry_attempt":"0"},"application_tags":"unknown"}',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-Fb-Connection-Type': 'WIFI',
+                'X-Fb-Device-Group': str(random.randrange(4700, 5000)),
+                'Priority': 'u=3,i',
+                'Accept-Encoding': 'gzip, deflate',
+                'X-Fb-Http-Engine': 'Liger',
+                'X-Fb-Client-Ip': 'true',
+                'X-Fb-Server-Cluster': 'true',
+                'Content-Length': str(random.randrange(1500, 2000)),
+                'cache-control': "private, no-cache, no-store, must-revalidate",
+                "facebook-api-version": "v1.0",
+                "pragma": "no-cache",
+                "priority": "u=0,i",
+                "strict-transport-security": "max-age=15552000; preload",
+                "vary": "Accept-Encoding",
+                "x-fb-connection-quality": "GOOD; q=0.7, rtt=73, rtx=0, c=23, mss=1232, tbw=5012, tp=10, tpl=0, uplat=405, ullat=0",
+                "x-fb-debug": "g/lwUlHD6vXZly0pnMoWnhifQ8PoyIuzDnUKVk5ZWru6+2XT2yaUB9Y/TSXbt0/637lElrllnUhGyXNJLheBKA==",
+                "x-fb-request-id": "AEJauAi2IHwyhd_zl3pC-4E",
+                "x-fb-rev": "1025308755",
+                "x-fb-trace-id": "C/GnaBOOeUa",
+                "x-frame-options": "DENY"
+            }
+
+            # Define data payload
+            data = {
+                'adid': str(uuid.uuid4()),
+                'format': 'json',
+                'device_id': str(uuid.uuid4()),
+                'email': '',  # To be filled
+                'password': '',  # To be filled
+                'generate_analytics_claim': '1',
+                'community_id': '',
+                'linked_guest_account_userid': '',
+                'cpl': True,
+                'try_num': '1',
+                'family_device_id': str(uuid.uuid4()),
+                'secure_family_device_id': str(uuid.uuid4()),
+                'credentials_type': 'password',
+                'fb4a_shared_phone_cpl_experiment': 'fb4a_shared_phone_nonce_cpl_at_risk_v3',
+                'fb4a_shared_phone_cpl_group': 'enable_v3_at_risk',
+                'enroll_misauth': False,
+                'generate_session_cookies': '1',
+                'error_detail_type': 'button_with_disabled',
+                'source': 'login',
+                'machine_id': ''.join(
+                    [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in
+                     range(24)]),
+                'jazoest': str(random.randrange(22000, 23000)),
+                'meta_inf_fbmeta': 'V2_UNTAGGED',
+                'advertiser_id': str(uuid.uuid4()),
+                'currently_logged_in_userid': '1',
+                'locale': 'fil_PH',
+                'client_country_code': 'PH',
+                'fb_api_req_friendly_name': 'authenticate',
+                'fb_api_caller_class': 'Fb4aAuthHandler',
+                'api_key': '882a8490361da98702bf97a021ddc14d',
+                'sig': hashlib.md5(str(uuid.uuid4()).encode()).hexdigest()[:32],
+                'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+            }
+
+            data['email'] = email_or_phone.strip()
+            data['password'] = f'#PWD_FB4A:0:{str(time.time())[:10]}:{password.strip()}'
+            pos = r.post('https://b-graph.facebook.com/auth/login', data=data, headers=head).json()
+
+            if ('session_key' in str(pos)) and ('access_token' in str(pos)):
+                token = pos['access_token']
+            else:
+                if 'error' in pos:
+                    print(f'{COLOR_RED}Error Message:{COLOR_RESET} {pos["error"]["error_user_title"]}')
+                else:
+                    print(f'{COLOR_RED}Unknown error format:{COLOR_RESET} {pos}')
+
+
+            data_to_save = [full_name, email_or_phone, password, profile_id, token]
             save_to_xlsx(filename_xlsx, data_to_save)  # Assuming filename_xlsx and save_to_xlsx are defined
             save_to_txt(filename_txt, data_to_save)  # Assuming filename_txt and save_to_txt are defined
             print(f"✅ Account saved | {full_name}")
