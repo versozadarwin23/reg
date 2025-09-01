45e9d03f4cfe13eaca9d4f21cd342930f2fcf470
versozadarwin23 <109713017+versozadarwin23@users.noreply.github.com>
2025-09-01 20:40:19 +0800

    Update reg.py

--- reg.py
+++ reg.py
@@ -289,26 +289,8 @@ def save_to_txt(filename, data):
     except Exception as e:
         print(f"\033[1;91m❗ Error saving to {filename}: {e}\033[0m")
 
-def has_access_token_in_xlsx(filename, email_address):
-    if not os.path.exists(filename):
-        return False
-
-    try:
-        wb = load_workbook(filename)
-    except BadZipFile:
-        print(f"\033[91m⚠️ Corrupted XLSX detected at {filename}. Skipping access token check.\033[0m")
-        return False
-
-    ws = wb.active
-    for row in ws.iter_rows(min_row=2, values_only=True):
-        saved_email = row[1]
-        saved_access_token = row[4]
-        if saved_email == email_address and saved_access_token and saved_access_token.strip():
-            return True
-    return False
-
 def save_to_xlsx(filename, data):
-    header_columns = ['NAME', 'USERNAME', 'PASSWORD', 'ACCOUNT LINK', 'ACCESS TOKEN']
+    header_columns = ['NAME', 'USERNAME', 'PASSWORD', 'ACCOUNT LINK']
 
     while True:
         try:
@@ -478,6 +460,8 @@ def create_fbunconfirmed(account_type, usern, gender, password=None, session=Non
         print(f"\033[92mUsing generated phone number:\033[0m {email_or_phone}")
         is_phone_choice = True
 
+    clear_console()
+
     data = {
         "firstname": firstname,
         "lastname": lastname,
@@ -571,114 +555,20 @@ def create_fbunconfirmed(account_type, usern, gender, password=None, session=Non
     filename_xlsx = "/storage/emulated/0/Acc_Created.xlsx"
     filename_txt = "/storage/emulated/0/Acc_created.txt"
 
-    while True:
-        save_confirm = input("Do you want to save this account (y/n): ").lower()
-        if save_confirm == 'y':
+    save_confirm = input("Do you want to save this account (y/n): ").lower()
 
-            clear_console()
+    if save_confirm == 'y':
+        clear_console()
+        data_to_save = [full_name, email_or_phone, password, profile_id]
+        save_to_xlsx(filename_xlsx, data_to_save)
+        save_to_txt(filename_txt, data_to_save)
+        print(f"✅ Account saved | {full_name}")
+    elif save_confirm == 'n':
+        print("Account not saved.")
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
+    clear_console()
 
-            r = requests.Session()
-
-            COLOR_GREEN = '\033[92m'
-            COLOR_RED = '\033[91m'
-            COLOR_YELLOW = '\033[93m'
-            COLOR_CYAN = '\033[96m'  # New color for exit message
-            COLOR_RESET = '\033[0m'  # Resets the color to default
-            head = {
-                'Host': 'b-graph.facebook.com',
-                'X-Fb-Connection-Quality': 'EXCELLENT',
-                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
-                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) vivo Y93 Build/O11019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/138.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/496.0.0.45.65;IABMV/1;]',
-                'X-Tigon-Is-Retry': 'false',
-                'X-Fb-Friendly-Name': 'authenticate',
-                'X-Fb-Connection-Bandwidth': str(random.randrange(70000000, 80000000)),
-                'Zero-Rated': '0',
-                'X-Fb-Net-Hni': str(random.randrange(50000, 60000)),
-                'X-Fb-Sim-Hni': str(random.randrange(50000, 60000)),
-                'X-Fb-Request-Analytics-Tags': '{"network_tags":{"product":"350685531728","retry_attempt":"0"},"application_tags":"unknown"}',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'X-Fb-Connection-Type': 'WIFI',
-                'X-Fb-Device-Group': str(random.randrange(4700, 5000)),
-                'Priority': 'u=3,i',
-                'Accept-Encoding': 'gzip, deflate',
-                'X-Fb-Http-Engine': 'Liger',
-                'X-Fb-Client-Ip': 'true',
-                'X-Fb-Server-Cluster': 'true',
-                'Content-Length': str(random.randrange(1500, 2000)),
-                'cache-control': "private, no-cache, no-store, must-revalidate",
-                "facebook-api-version": "v1.0",
-                "pragma": "no-cache",
-                "priority": "u=0,i",
-                "strict-transport-security": "max-age=15552000; preload",
-                "vary": "Accept-Encoding",
-                "x-fb-connection-quality": "GOOD; q=0.7, rtt=73, rtx=0, c=23, mss=1232, tbw=5012, tp=10, tpl=0, uplat=405, ullat=0",
-                "x-fb-debug": "g/lwUlHD6vXZly0pnMoWnhifQ8PoyIuzDnUKVk5ZWru6+2XT2yaUB9Y/TSXbt0/637lElrllnUhGyXNJLheBKA==",
-                "x-fb-request-id": "AEJauAi2IHwyhd_zl3pC-4E",
-                "x-fb-rev": "1025308755",
-                "x-fb-trace-id": "C/GnaBOOeUa",
-                "x-frame-options": "DENY"
-            }
-
-            # Define data payload
-            data = {
-                'adid': str(uuid.uuid4()),
-                'format': 'json',
-                'device_id': str(uuid.uuid4()),
-                'email': '',  # To be filled
-                'password': '',  # To be filled
-                'generate_analytics_claim': '1',
-                'community_id': '',
-                'linked_guest_account_userid': '',
-                'cpl': True,
-                'try_num': '1',
-                'family_device_id': str(uuid.uuid4()),
-                'secure_family_device_id': str(uuid.uuid4()),
-                'credentials_type': 'password',
-                'fb4a_shared_phone_cpl_experiment': 'fb4a_shared_phone_nonce_cpl_at_risk_v3',
-                'fb4a_shared_phone_cpl_group': 'enable_v3_at_risk',
-                'enroll_misauth': False,
-                'generate_session_cookies': '1',
-                'error_detail_type': 'button_with_disabled',
-                'source': 'login',
-                'machine_id': ''.join(
-                    [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in
-                     range(24)]),
-                'jazoest': str(random.randrange(22000, 23000)),
-                'meta_inf_fbmeta': 'V2_UNTAGGED',
-                'advertiser_id': str(uuid.uuid4()),
-                'currently_logged_in_userid': '1',
-                'locale': 'fil_PH',
-                'client_country_code': 'PH',
-                'fb_api_req_friendly_name': 'authenticate',
-                'fb_api_caller_class': 'Fb4aAuthHandler',
-                'api_key': '882a8490361da98702bf97a021ddc14d',
-                'sig': hashlib.md5(str(uuid.uuid4()).encode()).hexdigest()[:32],
-                'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-            }
-
-            data['email'] = email_or_phone.strip()
-            data['password'] = f'#PWD_FB4A:0:{str(time.time())[:10]}:{password.strip()}'
-            pos = r.post('https://b-graph.facebook.com/auth/login', data=data, headers=head).json()
-
-            if ('session_key' in str(pos)) and ('access_token' in str(pos)):
-                token = pos['access_token']
-            else:
-                if 'error' in pos:
-                    print(f'{COLOR_RED}Error Message:{COLOR_RESET} {pos["error"]["error_user_title"]}')
-                else:
-                    print(f'{COLOR_RED}Unknown error format:{COLOR_RESET} {pos}')
-
-
-            data_to_save = [full_name, email_or_phone, password, profile_id, token]
-            save_to_xlsx(filename_xlsx, data_to_save)  # Assuming filename_xlsx and save_to_xlsx are defined
-            save_to_txt(filename_txt, data_to_save)  # Assuming filename_txt and save_to_txt are defined
-            print(f"✅ Account saved | {full_name}")
-            break
-        elif save_confirm == 'n':
-            print("Account not saved.")
-            break
-        else:
-            print("Invalid input. Please enter 'y' or 'n'.")
 
 def NEMAIN():
     clear_console()
@@ -697,8 +587,8 @@ def NEMAIN():
         create_fbunconfirmed(account_type, usern, gender, session=session)
 
 if __name__ == "__main__":
-    if os.path.exists("/storage/emulated/0/settings.json"):
-        os.remove("/storage/emulated/0/settings.json")
+    if os.path.exists("settings.json"):
+        os.remove("settings.json")
     while True:
         clear_console()
         NEMAIN()
